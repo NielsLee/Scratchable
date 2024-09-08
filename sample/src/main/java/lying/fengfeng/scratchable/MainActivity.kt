43b7d21b4229b9f91011acd8a522b5ff9859aa2e
fengfeng <Niels_Lee@outlook.com>
2024-09-08 17:20:24 +0800

    Finished Scratchable composable

--- sample/src/main/java/lying/fengfeng/scratchable/MainActivity.kt
+++ sample/src/main/java/lying/fengfeng/scratchable/MainActivity.kt
@@ -1,16 +1,24 @@
 package lying.fengfeng.scratchable
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import lying.fengfeng.scratchable.ui.theme.ScratchableTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +50,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ScratchableTheme {
-        Greeting("Android")
+        Scratchable {
+            Column {
+                Greeting("Android")
+                Greeting("Android")
+                Greeting("Android")
+                Greeting("Android")
+                Greeting("Android")
+                Button(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Info, contentDescription = null)
+                }
+            }
+        }
     }
 }
--- scratchable/src/main/java/lying/fengfeng/scratchable/Scratchable.kt
+++ scratchable/src/main/java/lying/fengfeng/scratchable/Scratchable.kt
@@ -1,10 +1,157 @@
 package lying.fengfeng.scratchable
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathOperation
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+import kotlin.math.PI
 
+
+/**
+ * A container used to add a scratchable surface upon content
+ *
+ * @param modifier the modifier to be applied to this layout
+ * @param scratcherRadius radius of the circular scratcher
+ * @param cover the color of the cover surface
+ * @param coverExitTransition exit transition of the cover surface when reached moving threshold
+ * @param movingThresholdMillis determines how long this container takes to display its entire contents when scratching
+ * @param onFinished callback when reached moving threshold
+ * @param content content of this container
+ */
 @Composable
 fun Scratchable(
-
+    modifier: Modifier = Modifier,
+    scratcherRadius: Dp = 10.dp,
+    cover: Color = Color.Gray,
+    movingThresholdMillis: Long = 1000,
+    coverExitTransition: ExitTransition = fadeOut(),
+    onFinished: (() -> Unit)? = null,
+    content: @Composable () -> Unit
 ) {
+    var contentSize by remember { mutableStateOf(Size(0f, 0f)) }
 
+    var offset by remember { mutableStateOf(listOf<Offset>()) }
+    var totalArea by remember { mutableFloatStateOf(Float.MAX_VALUE) }
+    var scratchedArea by remember { mutableFloatStateOf(0f) }
+    var isScratching by remember { mutableStateOf(false) }
+    var lastMoveTimestamp = 0L
+    var movingMillis = 0L
+    val coverVisible by remember {
+        derivedStateOf {
+            if (isScratching) {
+                true
+            } else {
+                movingMillis  < movingThresholdMillis
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = coverVisible) {
+        if (!coverVisible) onFinished?.invoke()
+    }
+
+    Box(
+        modifier = modifier
+            .onSizeChanged { newSize ->
+                contentSize = newSize.toSize()
+                totalArea = contentSize.width * contentSize.height
+            }
+    ) {
+        content()
+    }
+
+    AnimatedVisibility(
+        visible = coverVisible,
+        exit = coverExitTransition
+    ) {
+        Canvas(
+            modifier = modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent()
+
+                            event.changes.forEach { inputChange ->
+                                offset = offset
+                                    .toMutableList()
+                                    .apply { add(inputChange.position) }
+                                inputChange.consume()
+
+                                val radiusPx = scratcherRadius.toPx()
+                                scratchedArea += (PI * radiusPx * radiusPx).toFloat()
+                            }
+
+                            when (event.type) {
+                                PointerEventType.Press -> {
+                                    isScratching = true
+                                    lastMoveTimestamp = System.currentTimeMillis()
+                                }
+
+                                PointerEventType.Move -> {
+                                    // calculate how long we scratching
+                                    movingMillis += System.currentTimeMillis() - lastMoveTimestamp
+                                    lastMoveTimestamp = System.currentTimeMillis()
+                                }
+
+                                PointerEventType.Release -> {
+                                    isScratching = false
+                                }
+                            }
+                        }
+                    }
+                },
+        ) {
+
+            // create cover path
+            val coverPath = Path().apply {
+                addRect(Rect(0f, 0f, contentSize.width, contentSize.height))
+            }
+
+            val radiusPx = scratcherRadius.toPx()
+            val scratchedPath = Path().apply {
+                offset.forEach { offset ->
+                    addOval(
+                        Rect(
+                            offset.x - radiusPx, offset.y - radiusPx,
+                            offset.x + radiusPx, offset.y + radiusPx
+                        )
+                    )
+                }
+            }
+
+            val resultPath = Path().apply {
+                addPath(coverPath)
+                op(this, scratchedPath, PathOperation.Difference)
+            }
+
+            drawPath(
+                path = resultPath,
+                color = cover
+            )
+        }
+    }
 }
